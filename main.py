77692fbe588fbda3851aee4469ba935f2cf5d5b4
koxan <koxanovskij86@mail.ru>
2022-09-09 20:34:53 +0300

    Бот обращается к базе данных с расписанием занятий. Осталось разместить на сервере и добавить авторизацию пользователей бота.

--- main.py
+++ main.py
@@ -1,12 +1,52 @@
 import pickle
 import re
+import sqlite3
 
-# with open('news_list.data', 'rb') as f:
-#    file = pickle.load(f)
-#
-#
-# for i in range(len(file)):
-#     print(file[i])
-#     print()
+conn = sqlite3.connect('studies.db')
+
+# Создаем курсор
+cursor = conn.cursor()
+
+cursor.execute('SELECT * from lessons')
+
+# cursor.execute('SELECT * from lessons where date=?', ('2022-09-08',))
+
+cursor.execute(
+    'select l.date, d.name, ct.class from lessons l, disciplines d, class_type ct where l.first_lesson = d.id and l.first_lesson_type = ct.id ')
+
+first_lesson_res = cursor.fetchall()
+
+cursor.execute(
+    'select l.date, d.name, ct.class from lessons l, disciplines d, class_type ct where l.second_lesson = d.id and l.second_lesson_type = ct.id')
+
+second_lesson_res = cursor.fetchall()
+
+cursor.execute(
+    'select l.date, d.name, ct.class from lessons l, disciplines d, class_type ct where l.third_lesson = d.id and l.third_lesson_type = ct.id')
+
+third_lesson_res = cursor.fetchall()
+
+sum_lessons_res = []
 
 
+# for i in second_lesson_res:
+#     print(i)
+
+
+def concat_lesson(first_lessons, second_lessons, third_lesson):
+    res_lessons = []
+
+    for k in range(len(first_lessons)):
+        if first_lessons[k][0] == second_lessons[k][0] == third_lesson[k][0]:
+            day_lessons = [first_lessons[k][0], str(first_lessons[k][1]) + '(' + str(first_lessons[k][2]) + ')',
+                           str(second_lessons[k][1]) + '(' + str(second_lessons[k][2]) + ')',
+                           str(third_lesson[k][1]) + '(' + str(third_lesson[k][2]) + ')']
+            res_lessons.append(day_lessons)
+
+    return res_lessons
+
+
+all_lessons = concat_lesson(first_lesson_res, second_lesson_res, third_lesson_res)
+
+for i in all_lessons:
+    print(i)
--- mo_bot.py
+++ mo_bot.py
@@ -1,6 +1,8 @@
 import telebot
 from HTTP_parser import take_news_to_list
 from telebot import types
+from main import all_lessons
+import sqlite3
 
 with open('token.txt', 'r') as f:
     token = f.read()
@@ -15,54 +17,63 @@ def send_welcome(message):
 
 
 @bot.message_handler(commands=['info'])
-def send_welcome(message):
+def show_info(message):
     bot.reply_to(message,
                  f'Бот предназначен для побликации подборки новостей по теме "МО РФ" с новостоного портала РБК')
 
 
-@bot.message_handler(commands=['news'])
-def send_welcome(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("Все новости")
-    item2 = types.KeyboardButton("10 последних новостей")
-    item3 = types.KeyboardButton("5 последних новостей")
+@bot.message_handler(commands=['learning'])
+def take_timetable(message):
+    markup = types.InlineKeyboardMarkup()
+    item1 = types.InlineKeyboardButton(text='Расписание на сегодня', callback_data='today')
+    item2 = types.InlineKeyboardButton(text='Расписание на завтра', callback_data='tomorrow')
+    item3 = types.InlineKeyboardButton(text='Расписание сессии', callback_data='session')
+
     markup.add(item1)
     markup.add(item2)
     markup.add(item3)
+
+    bot.send_message(message.chat.id, 'Что вы хотите посмотреть?', reply_markup=markup)
+
+
+@bot.message_handler(commands=['news'])
+def send_welcome(message):
+    markup = types.InlineKeyboardMarkup()
+    item1 = types.InlineKeyboardButton(text="Все новости", callback_data='all')
+    item2 = types.InlineKeyboardButton(text="10 последних новостей", callback_data='ten')
+    item3 = types.InlineKeyboardButton(text="5 последних новостей", callback_data='five')
+    markup.add(item1)
+    markup.add(item2)
+    markup.add(item3)
+
     bot.send_message(message.chat.id, 'Сколько показать новостей?', reply_markup=markup)
 
 
-
-
-
-@bot.message_handler(commands=['ten_news'])
-def send_welcome(message):
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
     new_list = take_news_to_list()
-    for one_news in reversed(new_list[:10]):
-        bot.reply_to(message, f'{one_news.news_title} {one_news.news_url}')
-
-
-@bot.message_handler(commands=['five_news'])
-def send_welcome(message):
-    new_list = take_news_to_list()
-    for one_news in reversed(new_list[:5]):
-        bot.reply_to(message, f'{one_news.news_url}')
+    if call.message:
+        if call.data == 'all':
+            for one_news in reversed(new_list):
+                bot.send_message(call.message.chat.id, f'{one_news.news_url}')
+        elif call.data == 'ten':
+            for one_news in reversed(new_list[:10]):
+                bot.send_message(call.message.chat.id, f'{one_news.news_title} {one_news.news_url}')
+        elif call.data == 'five':
+            for one_news in reversed(new_list[:5]):
+                bot.send_message(call.message.chat.id, f'{one_news.news_title} {one_news.news_url}')
+        elif call.data == 'session':
+            lessons = all_lessons
+            date = str(lessons[0][0])
+            first_lesson = 'первая пара - ' + str(lessons[0][1])
+            second_lesson = 'вторая пара - ' + str(lessons[0][2])
+            third_lesson = 'третья пара - ' + str(lessons[0][3])
+            bot.send_message(call.message.chat.id, f'{date}\n{first_lesson}\n{second_lesson}\n{third_lesson}')
 
 
 @bot.message_handler(content_types=['text'])
 def about_tex(message):
-    new_list = take_news_to_list()
-    if message.text.strip() == 'Все новости':
-        for one_news in reversed(new_list):
-            bot.reply_to(message, f'{one_news.news_url}')
-    elif message.text.strip() == '10 последних новостей':
-        for one_news in reversed(new_list[:10]):
-            bot.reply_to(message, f'{one_news.news_title} {one_news.news_url}')
-    elif message.text.strip() == '5 последних новостей':
-        for one_news in reversed(new_list[:5]):
-            bot.reply_to(message, f'{one_news.news_title} {one_news.news_url}')
-    else:
-        bot.reply_to(message, f'Команда - "{message.text}" не предусмотрена программой данного бота')
+    bot.reply_to(message, f'Команда - "{message.text}" не предусмотрена программой данного бота')
 
 
 bot.polling(none_stop=True)
